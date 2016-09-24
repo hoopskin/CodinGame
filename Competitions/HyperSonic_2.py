93e180006280f31659b12bef58febdb6c7d8dd42
hoopskin <hoopskin@gmail.com>
2016-09-24 17:46:35 -0500

    Currently finding best spot, going there while constantly calling 'BOMB'

--- Competitions/HyperSonic_2.py
+++ Competitions/HyperSonic_2.py
@@ -3,12 +3,12 @@ import math
 
 class Entity(object):
 	def __init__(self, entity_type, owner, x, y, param_1, param_2):
-    	#entity_type = id of the player (0 for player or 1 for bomb)
-    	#owner = id of the bomb owner
-    	#x = x
-    	#y = y
-    	#param_1 = Players: Numbers of bombs a player can still place. Bombs: Rounds left until explosion
-    	#param_2 = Players: Explosion range of player's bombs. Bombs: Explosion range.
+		#entity_type = id of the player (0 for player or 1 for bomb)
+		#owner = id of the bomb owner
+		#x = x
+		#y = y
+		#param_1 = Players: Numbers of bombs a player can still place. Bombs: Rounds left until explosion
+		#param_2 = Players: Explosion range of player's bombs. Bombs: Explosion range.
 		super(Entity, self).__init__()
 		self.entity_type = entity_type
 		self.owner = owner
@@ -25,17 +25,78 @@ box = '0'
 roundsRemaining = 200
 
 #Bomb Range = 3
-#Bomb hitting bomb means bomb explodes (bomb)
+#Bomb hitting bomb means bomb explodes (say bomb again. I dare you.)
+
+def getImpact(x, y):
+	impact = 0
+	#North
+	for i in range(1,4):
+		if y-i < 0:
+			break
+		else:
+			if gameMap[y-i][x] == box:
+				impact+=1
+				break
+
+	#South
+	for i in range(1,4):
+		if y+i >= height:
+			break
+		else:
+			if gameMap[y+i][x] == box:
+				impact+=1
+				break
+
+	#East
+	for i in range(1,4):
+		if x+i >= width:
+			break
+		else:
+			if gameMap[y][x+i] == box:
+				impact+=1
+				break
+
+	#West
+	for i in range(1,4):
+		if x-i < 0:
+			break
+		else:
+			if gameMap[y][x-i] == box:
+				impact+=1
+				break
+
+	return impact
+
+def getBestLoc():
+	x = 0
+	y = 0
+	maxImpact = 0
+	maxX = -1
+	maxY = -1
+	for x in range(width):
+		for y in range(height):
+			impact = getImpact(x, y)
+			if impact > maxImpact:
+				maxX = x
+				maxY = y
+				maxImpact = impact
+	return maxX, maxY
+
+def getMyBombObj():
+	for e in entityObjects:
+		if e.entity_type == 1 and e.owner = my_id:
+			return e
 
 # game loop
 while True:
 	entityObjects = []
-    for i in range(height):
-        row = input()
-    entities = int(input())
-    for i in range(entities):
-        entity_type, owner, x, y, param_1, param_2 = [int(j) for j in input().split()]
-        entityObjects.append(Entity(entity_type, owner, x, y, param_1, param_2))
+	for i in range(height):
+		row = input()
+		gameMap.append(list(row))
+	entities = int(input())
+	for i in range(entities):
+		entity_type, owner, x, y, param_1, param_2 = [int(j) for j in input().split()]
+		entityObjects.append(Entity(entity_type, owner, x, y, param_1, param_2))
 
-
-    print("BOMB 6 5")
+	bestX, bestY = getBestLoc()
+	print("BOMB %i %i" % (bestX, bestY))
